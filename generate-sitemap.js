e264afdfbacdefbfbd52653e5b596099412437c4
Holy1-2 <amanitoprayhirwa@gmail.com>
2026-03-09 20:00:24 +0200

    convert from base to real urls

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -8,11 +8,11 @@ dotenv.config();
 // Paste your Firebase Config here (same as in your App)
 const firebaseConfig = {
   apiKey: VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID
 };
 
 const BASE_URL = 'https://kubananimanaburimunsi.vercel.app';
